a8674a6dd41418566bc43286a5823931ca1f303a
dule <d@miletich.cc>
2019-07-05 22:47:00 +0200

    Add entry files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var root = document.getElementById('root');
+var title = document.createTextNode('Hello, World');
+if (root)
+    root.appendChild(title);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const root = document.getElementById('root');
+const title = document.createTextNode('Hello, World');
+
+if (root) root.appendChild(title);
